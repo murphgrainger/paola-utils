1fd7d60647314aeb75c86b098c38bc7d2535c433
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-10-22 11:38:14 -0600

    added additional student information fields to sfdc query

--- index.js
+++ index.js
@@ -7,36 +7,6 @@ const GitHub = require('./github');
 const Learn = require('./learn');
 const Slack = require('./slack');
 
-// const subjectQuery = 'upcoming deadline';
-// const toList = ['paola@galvanize.com'];
-// const ccList = [];
-// const bccList = [];
-// const mergeFields = {
-//   name: 'Bob',
-//   deadline: 'Nov 1, 2020',
-//   part: 99,
-//   days: 2,
-// };
-// const alias = { name: 'SEI Precourse', email: 'sei.precourse@galvanize.com' };
-// GMail.sendEmailFromDraft(subjectQuery, toList, ccList, bccList, alias, mergeFields);
-
-// ------------------------------
-// API Integrations
-// ------------------------------
-// Prerequisites and Limitations:
-//
-//  ⁃ Get API credentials for each API
-//  ⁃ Determine rate limits of each API
-//  ⁃ Make a list of required docs page links to inform each function’s implementation
-//
-//  TDD:
-//
-//  ⁃ Write tests that verify success for each function
-//  ⁃ Write tests to verify successful handling of errors for each function
-
-// TODO: Ultimately, we'll remove helloWorld. Just a test to make sure the NPM module is working :)
-exports.helloWorld = () => 'Hello World, I\'m Paola!';
-
 exports.GSheets = GSheets;
 exports.GGroups = GGroups;
 exports.GMail = GMail;
